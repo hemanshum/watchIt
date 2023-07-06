49a307b4b50a1fef55c1277e6a1eb675a0e06157
Hemanshu <hemanshu.mahajan@gmail.com>
2023-07-06 21:38:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+const debounce = require('lodash.debounce')
+const chokidar = require('chokidar');
+const program = require('caporal');
+
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action((args) => {
+    console.log(args)
+  });
+
+  program.parse(process.argv)
+
+// const start = debounce(() => {
+//   console.log('starting user program')
+// }, 100);
+
+// chokidar
+//   .watch('.')
+//   .on('add', start)
+//   .on('change', () => console.log('File changed'))
+//   .on('unlink', () => console.log('File unlinked'));
