41f34c1dfbae0f16a5ddf8ccd445de2cf5fbc4cf
Hemanshu <hemanshu.mahajan@gmail.com>
2023-07-07 04:56:35 +0530

    completed

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 #!/usr/bin/env node
-const debounce = require('lodash.debounce')
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
+const chalk = require('chalk');
 
 program
   .version('0.0.1')
   .argument('[filename]', 'Name of a file to execute')
-  .action((args) => {
-    console.log(args)
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
+    try {
+      await fs.promises.access(name);
+    } catch (error) {
+      throw new Error('Could not find the file ' + name);
+    }
+
+    let proc;
+    const start = debounce(() => {
+      if (proc) {
+        proc.kill();
+      }
+      console.log(chalk.blue('>>> Starting process...'));
+      proc = spawn('node', [name], { stdio: 'inherit' });
+    }, 100);
+
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
   });
 
-  program.parse(process.argv)
-
-// const start = debounce(() => {
-//   console.log('starting user program')
-// }, 100);
-
-// chokidar
-//   .watch('.')
-//   .on('add', start)
-//   .on('change', () => console.log('File changed'))
-//   .on('unlink', () => console.log('File unlinked'));
+program.parse(process.argv);
---
+++ test.js
@@ -0,0 +1,3 @@
+setInterval(() => {
+  console.log('Bye there!');
+}, 1000);
